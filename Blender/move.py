45d11e98f2ce7be1b982c19ae29973a68396feda
Eugene de Beste <banshee1221@gmail.com>
2015-09-12 15:53:51 +0200

    Progress! For loops working

--- Blender/move.py
+++ Blender/move.py
@@ -1,38 +1,67 @@
 import bge
 import bpy
 import mathutils
+import time
 
-x = 0
-y = 0
-
-def moveUnitOne(obj, dir):
-	global x
-    global y
-	 
-	if dir is "n":
-        y = 1
-    elif dir is "s":
-        y = -1
-    elif dir is "e":
-        x = 1
-    elif dir is "w":
-        x = -1
+class move:
+	def __init__(self):
+		#pass
+		self.moving = False
+		self.endTime = 0
+		self.startTime = 0
+		self.endPos = []
+		self.startPos = []
 		
-	obj.worldPosition = [obj.position.x + x,obj.position.y + y,obj.position.z]
+	def moveUnitOne(self, obj, dir):
 		
-def moveUnit(obj, dir, steps):
-    global x
-    global y
-    
-    
-    if dir is "n":
-        y = steps
-    elif dir is "s":
-        y = -steps
-    elif dir is "e":
-        x = steps
-    elif dir is "w":
-        x = -steps
-        
-    #obj.setLinearVelocity([y, x, 0], True)
-    obj.worldPosition = [obj.position.x + x,obj.position.y + y,obj.position.z]
+		x = 0
+		y = 0
+		 
+		if dir is "n":
+			y = 1
+		elif dir is "s":
+			y = -1
+		elif dir is "e":
+			x = 1
+		elif dir is "w":
+			x = -1
+			
+		
+		if (self.moving == False):
+			self.startTime = time.time()
+			self.endTime = self.startTime + 0.25
+			self.moving = True
+			self.endPos = [obj.position.x + x,obj.position.y + y,obj.position.z]
+			self.startPos = [obj.position.x, obj.position.y, obj.position.z]
+		
+		percent = (time.time() - self.startTime) * 4
+		if (percent < 1):
+			print(percent)
+			obj.worldPosition = lerp(self.startPos, self.endPos, percent)
+		else:
+			percent = 1
+			obj.worldPosition = lerp(self.startPos, self.endPos, percent)
+			self.moving = False
+		#obj.worldPosition = [obj.position.x + x,obj.position.y + y,obj.position.z]
+		return percent
+		
+	def moveUnit(self, obj, dir, steps):
+		x = 0
+		y = 0
+		
+		if dir is "n":
+			y = steps
+		elif dir is "s":
+			y = -steps
+		elif dir is "e":
+			x = steps
+		elif dir is "w":
+			x = -steps
+			
+		#obj.setLinearVelocity([y, x, 0], True)
+		obj.worldPosition = [obj.position.x + x,obj.position.y + y,obj.position.z]
+		
+def lerp(startPos, endPos, percent):
+	x = startPos[0] + percent*(endPos[0] - startPos[0])
+	y = startPos[1] + percent*(endPos[1] - startPos[1])
+	return([x, y, startPos[2]])
--- Blender/test.py
+++ Blender/test.py
@@ -11,54 +11,54 @@ class tester:
 		objList = scene.objects
 		obj_name = 'Cube'
 		self.Cube = objList[obj_name]
-		self.lines = []
-		self.counter = 0
-		self.timer = time.time()+1
+		self.text = ''
+		self.actions = []
 		self.passed = False
 		self.moved = False
+		self.m = move.move()
+		self.running = False
 		
 	def setText(self, stri):
-		#print(stri)
 		self.text = stri
-		self.lines = stri.split('\n')
 	
 	def run(self):
 				
 		error = -1
-		if self.counter < len(self.lines):
-	
+		if (self.text != ''):
 			print(str(self.text))
 			print(self.compile_check(str(self.text)))
 			print(valid_check(str(self.text)))
 
-		
-			#print(str(tmp.x) + " | " + str(tmp.y))
-
-			if self.timer < time.time():
-				print("Hello!")
-				self.timer = time.time()+1
-				try:
-					codeobj = compile(str(self.text), '', 'exec')
-					eval(codeobj, globals(), locals())
-				except AttributeError as e:
-					print(e)
-				self.counter += 1	
-		
-		
-		else:
-			self.counter = 0
-			self.lines = []
+			try:
+				codeobj = compile(str(self.text), '<string>', 'exec')
+				eval(codeobj, globals(), locals())
+				self.text = ''
+				print(self.actions)
+			except Exception as e:
+				print(e)
+				self.text = ''
+				
+		if (len(self.actions) > 0):
+			currItem = self.actions[0]
+			if list(currItem.items())[0][0] == 'move':
+				checker = self.m.moveUnitOne(self.Cube, list(currItem.items())[0][1])
+				if checker == 1:
+					del self.actions[0]
 			
 		return error
 		
 	def compile_check(self, code):
 		try:
-			compile(code, '<string>', 'single')
-		except SyntaxError as e:
+			compile(code, '<string>', 'exec')
+		except Exception as e:
 			print("Error executing code!\n=====================\nLine:\t{0}\nSnip:\t{1}\nIssue:\t{2}".format(e.lineno, e.text, e))
 			return False
 		
 		return True
+		
+	def move(self, dir):
+		self.actions.append({'move':dir})
+		#self.actions.append[{'open':password}]
 		
 def valid_check(code):
 	try:
